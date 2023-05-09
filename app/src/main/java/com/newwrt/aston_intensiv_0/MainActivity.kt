e639f0ddecd77bffaf835b9c24d2948891fb0443
Khuzin Rinat <r.khuzin@wrtech.ru>
2023-05-09 12:10:43 +0500

    check button fun

--- app/src/main/java/com/newwrt/aston_intensiv_0/MainActivity.kt
+++ app/src/main/java/com/newwrt/aston_intensiv_0/MainActivity.kt
@@ -2,12 +2,16 @@ package com.newwrt.aston_intensiv_0
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
-
-
+import androidx.annotation.IdRes
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val buttonCheckAge by bind<Button>(R.id.checkAgeBtn)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -15,25 +19,30 @@ class MainActivity : AppCompatActivity() {
 
         val catName = Cat("Marsik", 5)
 
-        catName.let { p ->
-            val result = when (p.age) {
-                in 1..9 -> Result.Young(p.name)
-                in 10..20 -> Result.MiddleAged(p.name)
-                else -> Result.Old(p.name)
-            }
+        buttonCheckAge.setOnClickListener {
+            catName.let { p ->
+                val result = when (p.age) {
+                    in 1..9 -> Result.Young(p.name)
+                    in 10..20 -> Result.MiddleAged(p.name)
+                    else -> Result.Old(p.name)
+                }
 
-            run {
-                val message = checkAge(result)
+                run {
+                    val message = checkAge(result)
 
-                message.also { msg ->
-                    Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                    message.also { msg ->
+                        Toast.makeText(this, msg, Toast.LENGTH_LONG).show()
+                    }
                 }
             }
         }
 
-        //finish()
+
     }
 
+    fun <T: Any?> bind(@IdRes idRes: Int): Lazy<T>{
+        return  lazy(LazyThreadSafetyMode.NONE) { findViewById<View>(idRes) as T }
+    }
 
 
     private fun checkAge(result: Result): String {
